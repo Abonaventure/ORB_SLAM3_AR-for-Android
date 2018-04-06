3b0b93bceeca205abe95fc667a30127d280fc1b0
Zhao <32775389+linfengzhao@users.noreply.github.com>
2018-04-07 00:17:07 +0800

    Add files via upload

---
+++ MyPersonalHomePage/js/time.js
@@ -0,0 +1,11 @@
+﻿function time()
+{
+    var d1 = new Date();
+    var time = "";
+    time = d1.toTimeString().split(' ')[0];
+    time += "  ";
+    time += d1.toLocaleDateString();
+     
+    var timeDiv = document.getElementById("time");
+    timeDiv.innerText = time;
+}
