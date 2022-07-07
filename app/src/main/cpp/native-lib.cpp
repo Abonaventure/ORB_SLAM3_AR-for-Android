1ee39a99db85c51908103ba1394a166d79661dc3
Abner <30580549+Abonaventure@users.noreply.github.com>
2022-07-07 22:48:24 +0800

    Update native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -62,7 +62,6 @@ cv::Point2f Camera2Pixel(cv::Mat poseCamera,cv::Mat mk){
 extern "C"
 JNIEXPORT jfloatArray JNICALL
 Java_com_vslam_orbslam3_vslamactivity_VslamActivity_CVTest(JNIEnv *env, jobject instance, jlong matAddr) {
-    LOGI("###gyj### come in CVTest!!!");
 #ifndef BOWISBIN
     if(tframe == 0)
     txt_2_bin();
@@ -70,30 +69,25 @@ Java_com_vslam_orbslam3_vslamactivity_VslamActivity_CVTest(JNIEnv *env, jobject 
 #else
 
     if(!SLAM)
-    {
-      SLAM = new ORB_SLAM3::System("/storage/emulated/0/SLAM/VOC/ORBvoc.bin","/storage/emulated/0/SLAM/Calibration/PARAconfig.yaml",ORB_SLAM3::System::MONOCULAR,false);
-        
+    { 
+        SLAM = new ORB_SLAM3::System("/storage/emulated/0/SLAM/VOC/ORBvoc.bin","/storage/emulated/0/SLAM/Calibration/PARAconfig.yaml",ORB_SLAM3::System::MONOCULAR,false);
+       //imageScale = SLAM->GetImageScale();
+       
     }
-
     cv::Mat *pMat = (cv::Mat*)matAddr;
     cv::Mat pose;
 
     std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
     tframe  = std::chrono::duration_cast < std::chrono::duration < double >> (t1 - t0).count();
-
     // Pass the image to the SLAM system
     cout << "tframe = " << tframe << endl;
     clock_t start,end;
     start=clock();
-    //cv::Mat im = pMat[0];
-    //cv::Mat im = *pMat;
-    //Sophus::SE3f Tcw_SE3f = SLAM->TrackMonocular(im,tframe); // TODO change to monocular_inertial
     Sophus::SE3f Tcw_SE3f = SLAM->TrackMonocular(*pMat,tframe); // TODO change to monocular_inertial
-    //Sophus::SE3f Tcw_SE3f = SLAM->TrackMonocular(*pMat,tframe,vImuMeas,filename); // TODO change to monocular_inertial
     Eigen::Matrix4f Tcw_Matrix = Tcw_SE3f.matrix();
     cv::eigen2cv(Tcw_Matrix, pose);
     end = clock();
-    LOGI("SLAM->TrackMonocular over!!!Get Pose Use Time=%f\n",((double)end-start)/CLOCKS_PER_SEC);
+    LOGI("Get Pose Use Time=%f\n",((double)end-start)/CLOCKS_PER_SEC);
 
     static bool instialized =false;
     static bool markerDetected =false;
@@ -103,57 +97,12 @@ Java_com_vslam_orbslam3_vslamactivity_VslamActivity_CVTest(JNIEnv *env, jobject 
     }
 
     if(!pose.empty()){
-//        if(markerDetected == false){
-//            Process process(pMat);
-//            markerDetected = process.Run();
-//            if(process.Run()){
-//                cv::Mat rwc,twc,tempTwc=cv::Mat::eye(4,4,CV_32F);
-//                rwc = pose.rowRange(0,3).colRange(0,3).t();
-//                twc = -rwc*pose.col(3).rowRange(0,3);
-//                rwc.copyTo(tempTwc.rowRange(0,3).colRange(0,3));
-//                twc.copyTo(tempTwc.col(3).rowRange(0,3));
-//                Marker2World = tempTwc*process.marker2origincamera;
-//            }
-//
-//        }else{
-//            vector<cv::Point3f> drawPoints(4);
-//            drawPoints[0] = cv::Point3f(0,0,1);
-//            drawPoints[1] = cv::Point3f(0.3,0,1);
-//            drawPoints[2] = cv::Point3f(0,0.3,1);
-//            drawPoints[3] = cv::Point3f(0,0,1.3);
-//            cv::Mat Marker2CameraNow = pose*Marker2World;
-//            cv::Mat rcm,tcm;
-//            cv::Rodrigues(Marker2CameraNow.rowRange(0,3).colRange(0,3),rcm);
-//            tcm = Marker2CameraNow.col(3).rowRange(0,3);
-//            std::vector<cv::Point2f> markerPoints;
-//            cv::projectPoints(drawPoints, rcm, tcm, SLAM.mpTracker->mK, SLAM.mpTracker->mDistCoef, markerPoints);
-//
-//            cv::line(*pMat, markerPoints[0],markerPoints[1], cv::Scalar(250, 0, 0), 5);
-//            cv::line(*pMat, markerPoints[0],markerPoints[2], cv::Scalar(0, 250, 0), 5);
-//            cv::line(*pMat, markerPoints[0],markerPoints[3], cv::Scalar(0, 0, 250), 5);
-//        }
-
-
-
-
         cv::Mat rVec;
         cv::Rodrigues(pose.colRange(0, 3).rowRange(0, 3), rVec);
         cv::Mat tVec = pose.col(3).rowRange(0, 3);
-
-        //const vector<ORB_SLAM3::MapPoint*> vpMPs = SLAM->mpTracker->mpMap->GetAllMapPoints();//所有的地图点
         const vector<ORB_SLAM3::MapPoint*> vpMPs = SLAM->mpTracker->mpAtlas->GetAllMapPoints();//所有的地图点
         const vector<ORB_SLAM3::MapPoint*> vpTMPs = SLAM->GetTrackedMapPoints();
         vector<cv::KeyPoint> vKPs = SLAM->GetTrackedKeyPointsUn();
-//        for(int i=0; i<vKPs.size(); i++)
-//        {
-//            if(vpTMPs[i])
-//            {
-//                cv::circle(*pMat, vKPs[i].pt, 2, cv::Scalar(0, 255, 0), 1, 8);
-//            }
-//        }
-//        if(vpTMPs.size() > 0){
-//
-//        }
         if (vpMPs.size() > 0) {
             std::vector<cv::Point3f> allmappoints;
             for (size_t i = 0; i < vpMPs.size(); i++) {
@@ -165,14 +114,10 @@ Java_com_vslam_orbslam3_vslamactivity_VslamActivity_CVTest(JNIEnv *env, jobject 
                     pos.x = mpos.at<float>(0);
                     pos.y = mpos.at<float>(1);
                     pos.z = mpos.at<float>(2) ;
-
-
-
                     allmappoints.push_back(pos);
-//                    LOGE("Point's world pose is %f %f %f",pos.x,pos.y,pos.z );
                 }
             }
-            LOGI("###gyj###all map points size %lu", allmappoints.size());
+        
             std::vector<cv::Point2f> projectedPoints;
             cv::projectPoints(allmappoints, rVec, tVec, SLAM->mpTracker->mK, SLAM->mpTracker->mDistCoef, projectedPoints);
             for (size_t j = 0; j < projectedPoints.size(); ++j) {
@@ -212,22 +157,6 @@ Java_com_vslam_orbslam3_vslamactivity_VslamActivity_CVTest(JNIEnv *env, jobject 
 
             }else{
                 cv::Mat Plane2Camera = pose*Plane2World;
-//                vector<cv::Mat> axisPoints(4);
-//                axisPoints[0] = (cv::Mat_ <float>(4,1)<<0,0,0,1);
-//                axisPoints[1] = (cv::Mat_ <float>(4,1)<<0.3,0,0,1);
-//                axisPoints[2] = (cv::Mat_ <float>(4,1)<<0,0.3,0,1);
-//                axisPoints[3] = (cv::Mat_ <float>(4,1)<<0,0,0.3,1);
-//                vector<cv::Point2f> drawPoints(4);
-//                for(int i = 0 ; i < 4; i++){
-//                    axisPoints[i] = Plane2Camera*axisPoints[i];
-//
-//                    drawPoints[i] = Camera2Pixel(axisPoints[i],SLAM.mpTracker->mK);
-//                    LOGE("drawPoints x y %f %f",drawPoints[i].x,drawPoints[i].y);
-//                }
-//                LOGE("axisPoints x y %f %f %f",axisPoints[0].at<float>(0,0),axisPoints[0].at<float>(1,0),axisPoints[0].at<float>(2,0));
-//                cv::line(*pMat, drawPoints[0],drawPoints[1], cv::Scalar(250, 0, 0), 5);
-//                cv::line(*pMat, drawPoints[0],drawPoints[2], cv::Scalar(0, 250, 0), 5);
-//                cv::line(*pMat, drawPoints[0],drawPoints[3], cv::Scalar(0, 0, 250), 5);
 
                 vector<cv::Point3f> drawPoints(8);
                 drawPoints[0] = cv::Point3f(0,0,0);
@@ -239,14 +168,6 @@ Java_com_vslam_orbslam3_vslamactivity_VslamActivity_CVTest(JNIEnv *env, jobject 
                 drawPoints[6] =cv::Point3f(0.3,0,0.3);
                 drawPoints[7] =cv::Point3f(0.3,0.3,0);
 
-
-
-//                for(int i = 0 ; i < 4 ;i ++){
-//                    drawPoints[i].x += centroid.at<float>(0,0);
-//                    drawPoints[i].y += centroid.at<float>(1,0);
-//                    drawPoints[i].z += centroid.at<float>(2,0);
-//                }
-
                 for(int row = 0 ; row < 4;row++){
                     LOGE(" Plane2Camera %f %f %f %f",Plane2Camera.at<float>(row,0),Plane2Camera.at<float>(row,1),Plane2Camera.at<float>(row,2),Plane2Camera.at<float>(row,3));
                 }
@@ -256,24 +177,7 @@ Java_com_vslam_orbslam3_vslamactivity_VslamActivity_CVTest(JNIEnv *env, jobject 
 
                 Tcp = Plane2Camera.col(3).rowRange(0,3);
                 LOGE("Tcp %f %f %f",Tcp.at<float>(0,0),Tcp.at<float>(1,0),Tcp.at<float>(2,0));
-//                cv::Rodrigues(pose.rowRange(0,3).colRange(0,3),Rcp);
-//                Tcp = pose.col(3).rowRange(0,3);
                 cv::projectPoints(drawPoints, Rcp, Tcp, SLAM->mpTracker->mK, SLAM->mpTracker->mDistCoef, projectedPoints);
-
-//                cv::line(*pMat, projectedPoints[0],projectedPoints[1], cv::Scalar(250, 0, 0), 5); //画X轴 红色
-//                cv::line(*pMat, projectedPoints[0],projectedPoints[2], cv::Scalar(0, 250, 0), 5);//画Z轴  绿色
-//                cv::line(*pMat, projectedPoints[0],projectedPoints[3], cv::Scalar(0, 0, 250), 5);//画Y轴 蓝色
-//                cv::line(*pMat, projectedPoints[1],projectedPoints[7], cv::Scalar(10, 0, 50), 2);
-//                cv::line(*pMat, projectedPoints[3],projectedPoints[7], cv::Scalar(20, 0, 50),2);
-//                cv::line(*pMat, projectedPoints[3],projectedPoints[4], cv::Scalar(30, 0, 50),2 );
-//                cv::line(*pMat, projectedPoints[2],projectedPoints[4], cv::Scalar(40, 0, 50),2);
-//                cv::line(*pMat, projectedPoints[1],projectedPoints[6], cv::Scalar(50, 0, 50), 2);
-//                cv::line(*pMat, projectedPoints[2],projectedPoints[6], cv::Scalar(60, 0, 50), 2);
-//                cv::line(*pMat, projectedPoints[4],projectedPoints[5], cv::Scalar(70, 0, 50), 2);
-//                cv::line(*pMat, projectedPoints[5],projectedPoints[6], cv::Scalar(80, 0, 50), 2);
-//                cv::line(*pMat, projectedPoints[5],projectedPoints[7], cv::Scalar(90, 0, 50), 2);
-
-//                cv::circle(*pMat, projectedPoints[0], 2, cv::Scalar(0, 0, 250), 1, 8);
 
             }
 
@@ -293,25 +197,10 @@ Java_com_vslam_orbslam3_vslamactivity_VslamActivity_CVTest(JNIEnv *env, jobject 
     }
 
     // Stop all threads
-    SLAM->Shutdown();
-
-    /*// Tracking time statistics
-    sort(vTimesTrack.begin(),vTimesTrack.end());
-    float totaltime = 0;
-    for(int ni=0; ni<nImages; ni++)
-    {
-        totaltime+=vTimesTrack[ni];
-    }
-    cout << "-------" << endl << endl;
-    cout << "median tracking time: " << vTimesTrack[nImages/2] << endl;
-    cout << "mean tracking time: " << totaltime/nImages << endl;
-*/
+    //SLAM->Shutdown();
     // Save camera trajectory
-    SLAM->SaveKeyFrameTrajectoryTUM("/storage/emulated/0/SLAM/Trajectory/KeyFrameTrajectory.txt");
+    //SLAM->SaveKeyFrameTrajectoryTUM("/storage/emulated/0/SLAM/Trajectory/KeyFrameTrajectory.txt");
 
-
-
-    /**将得到的相机位姿矩阵返回到java代码，以便后面更新opengl相机位姿**/
     cv::Mat ima=pose;
     jfloatArray resultArray = env->NewFloatArray(ima.rows * ima.cols);
     jfloat *resultPtr;
@@ -324,8 +213,6 @@ Java_com_vslam_orbslam3_vslamactivity_VslamActivity_CVTest(JNIEnv *env, jobject 
         }
     env->ReleaseFloatArrayElements(resultArray, resultPtr, 0);
    return resultArray;
-
-    LOGI("###gyj###Native Finished");
 #endif
 }
 
